fa3bdecf967e691dcabfe5d2aca24ec9f167a3c2
Max Walaschewski <mawalla@protonmail.com>
2020-09-10 12:10:18 +0200

    added random lead methods

--- src/common.h
+++ src/common.h
@@ -18,6 +18,9 @@ class CommonLEDs {
     void _randomLeadColor(float brightness);
     void _randomLeadGradient(float brightness);
     void _off(void);
+    //dirty but simple I guess
+    void _generateGradient(void);
+    float _generatedGradient[NUM_LEDS][3];
 };
 
 #endif
@@ -30,6 +33,27 @@ class CommonLEDs {
 
 StaticJsonDocument<16384> data;
 CRGB leds[NUM_LEDS];
+
+
+float randomLead(float input) {
+  return (input * 1.5 + random(0, 127)) / 2;
+}
+
+
+void CommonLEDs::_generateGradient() {
+  float r1 = data["input_color1"][0].as<float>() / NUM_LEDS;
+  float r2 = data["input_color2"][0].as<float>() / NUM_LEDS;
+  float g1 = data["input_color1"][1].as<float>() / NUM_LEDS;
+  float g2 = data["input_color2"][1].as<float>() / NUM_LEDS;
+  float b1 = data["input_color1"][2].as<float>() / NUM_LEDS;
+  float b2 = data["input_color2"][2].as<float>() / NUM_LEDS;
+
+  for(int i=0; i<NUM_LEDS; i++) {
+    _generatedGradient[i][0] = ((r2 * i * 2) + (r1 * (NUM_LEDS - 1 - i) * 2)) / 2;
+    _generatedGradient[i][1] = ((g2 * i * 2) + (g1 * (NUM_LEDS - 1 - i) * 2)) / 2;
+    _generatedGradient[i][2] = ((b2 * i * 2) + (b1 * (NUM_LEDS - 1 - i) * 2)) / 2;
+  }
+}
 
 
 CommonLEDs::CommonLEDs() {
@@ -50,31 +74,13 @@ void CommonLEDs::_singleColor(float brightness) {
 
 
 void CommonLEDs::_gradient(float brightness) {
-  float r1 = data["input_color1"][0].as<float>() / NUM_LEDS;
-  float r2 = data["input_color2"][0].as<float>() / NUM_LEDS;
-  float g1 = data["input_color1"][1].as<float>() / NUM_LEDS;
-  float g2 = data["input_color2"][1].as<float>() / NUM_LEDS;
-  float b1 = data["input_color1"][2].as<float>() / NUM_LEDS;
-  float b2 = data["input_color2"][2].as<float>() / NUM_LEDS;
-
-  float steps1[NUM_LEDS][3];
-  float steps2[NUM_LEDS][3];
-
-  for(int i=0; i<NUM_LEDS; i++) {
-    steps1[i][0] = r1 * i;
-    steps1[i][1] = g1 * i;
-    steps1[i][2] = b1 * i;
-
-    steps2[NUM_LEDS - 1 - i][0] = r2 * i;
-    steps2[NUM_LEDS - 1 - i][1] = g2 * i;
-    steps2[NUM_LEDS - 1 - i][2] = b2 * i;
-  }
+  _generateGradient();
 
   for(int i=0; i<NUM_LEDS; i++) {
     leds[i].setRGB(
-      (steps1[i][0] * 2 + steps2[i][0] * 2) / 2 * brightness,
-      (steps1[i][1] * 2 + steps2[i][1] * 2) / 2 * brightness,
-      (steps1[i][2] * 2 + steps2[i][2] * 2) / 2 * brightness
+      _generatedGradient[i][0] * brightness,
+      _generatedGradient[i][1] * brightness,
+      _generatedGradient[i][2] * brightness
     );
   }
 }
@@ -104,11 +110,25 @@ void CommonLEDs::_random(float brightness) {
 }
 
 void CommonLEDs::_randomLeadColor(float brightness) {
-
+  for(int i=0; i<NUM_LEDS; i++) {
+    leds[i].setRGB(
+      randomLead(data["input_color"][0].as<int>()) * brightness,
+      randomLead(data["input_color"][1].as<int>()) * brightness,
+      randomLead(data["input_color"][2].as<int>()) * brightness
+    );
+  }
 }
 
 void CommonLEDs::_randomLeadGradient(float brightness) {
+  _generateGradient();
 
+  for(int i=0; i<NUM_LEDS; i++) {
+    leds[i].setRGB(
+      randomLead(_generatedGradient[i][0]) * brightness,
+      randomLead(_generatedGradient[i][1]) * brightness,
+      randomLead(_generatedGradient[i][2]) * brightness
+    );
+  }
 }
 
 void CommonLEDs::_off() {
@@ -148,9 +168,9 @@ void CommonLEDs::display(char (&buffer)[BUFFERSIZE]) {
     _off(); 
   } else if(mode == "gradient") {
     _gradient(brightness); 
-  } else if(mode == "randomLeadColor") {
+  } else if(mode == "random_lead_color") {
     _randomLeadColor(brightness); 
-  } else if(mode == "randomLeadGradient") {
+  } else if(mode == "random_lead_gradient") {
     _randomLeadGradient(brightness); 
   }
 }
